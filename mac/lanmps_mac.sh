35f163f3e1d214ab35596f68900f9a33f97ef8ee
fox <80005368@qq.com>
2016-03-12 17:45:15 +0800

    苹果系统安装

--- mac/lanmps_mac.sh
+++ mac/lanmps_mac.sh
@@ -5,99 +5,13 @@ export PATH
 if [ $UID != 0 ]; then echo "Error: You must be root to run the install script, please use root to install lanmps";exit;fi
 IN_PWD=$(pwd)
 IN_DOWN=${IN_PWD}/down
-LOGPATH=${IN_PWD}/logs
-IN_DIR=${IN_PWD}"/lanmps"
-IN_WEB_DIR=${IN_PWD}"/wwwroot"
-IN_WEB_LOG_DIR=${IN_PWD}"/wwwLogs"
+IN_DIR=/Users/${USER}/lanmps
+IN_WEB_DIR={IN_PWD}/wwwroot
+IN_WEB_LOG_DIR=${IN_PWD}/wwwLogs
 
-mkdir -p $IN_DIR
-
-
-declare -A DPF;
-#http://cn2.php.net/distributions/php-5.6.19.tar.gz
-DPF['php5.6.x']="http://cn2.php.net/distributions/php-5.6.19.tar.gz"
-DPF['php5.6.x-V']="5.6.19"
-
-PHP_URL="http://cn2.php.net/distributions/php-5.6.19.tar.gz"
-
-#ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-8.38.tar.gz
-#https://sourceforge.net/projects/pcre/files/pcre/
-DPF['pcre']="ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-8.38.tar.gz"
-DPF['pcre-V']="8.38"
-
-
-if [ -s $PHP_URL ]; then
-    echo "php [found]"
-else
-    echo "Error: $IN_DOWN/$2 not found!!!download now......"
-    wget_down ${DUS[$t_name]}
-fi
-
-
-tar -xvf pcre-{$DPF['pcre-V']}.tar.gz
-
-./configure
-
-make
-
-make install
-
-
-groupadd www
-useradd -s /sbin/nologin -g www www
-
-
-tar zxvf php-${DPF['php5.6.x-V']}.tar.gz
-cd php-${DPF['php5.6.x-V']}/
-
-./configure --prefix="${IN_DIR}/php" \
---with-config-file-path="${IN_DIR}/php" \
---with-mysql=mysqlnd \
---with-mysqli=mysqlnd \
---with-pdo-mysql=mysqlnd \
---with-iconv-dir=/usr/local/libiconv \
---with-freetype-dir \
---with-jpeg-dir \
---with-png-dir \
---with-zlib \
---with-libxml-dir=/usr \
---enable-xml \
---enable-opcache \
---disable-rpath \
---enable-bcmath \
---enable-shmop \
---enable-sysvsem \
---enable-inline-optimization \
---with-curl \
---enable-mbregex \
---enable-mbstring \
---with-mcrypt \
---enable-ftp \
---with-gd \
---enable-gd-native-ttf \
---with-openssl \
---with-mhash \
---enable-pcntl \
---enable-sockets \
---with-xmlrpc \
---enable-zip \
---enable-soap \
---without-pear \
---with-gettext \
---disable-fileinfo --enable-fpm --with-fpm-user=www --with-fpm-group=www
-
-#make ZEND_EXTRA_LIBS='-liconv'
-make
-make install
-
-#sudo rm -rf /usr/bin/php
-#sudo rm -rf /usr/bin/php-config
-#sudo rm -rf /usr/bin/phpize
-
-
-sudo mv /usr/bin/php /usr/bin/php55
-sudo mv /usr/bin/php-config /usr/bin/php-config55
-sudo mv /usr/bin/phpize /usr/bin/phpize55
+mkdir -p $IN_DIR/vhost
+mkdir -p $IN_WEB_DIR
+mkdir -p $IN_WEB_LOG_DIR
 
 
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
@@ -107,6 +21,12 @@ brew tap homebrew/versions
 brew tap homebrew/php
 
 brew update
+
+#========================
+#PHP
+sudo mv /usr/bin/php /usr/bin/php55
+sudo mv /usr/bin/php-config /usr/bin/php-config55
+sudo mv /usr/bin/phpize /usr/bin/phpize55
 
 brew install php56 \
 --without-snmp \
@@ -124,54 +44,98 @@ brew install php56 \
 brew install php56-pdo-dblib \
 php56-pdo-pgsql \
 php56-mcrypt \
-php56-memcache \
-php56-memcached \
-php56-mongo \
+#php56-memcache \
+#php56-memcached \
+#php56-mongo \
 php56-redis \
-php56-xdebug \
-php56-wbxml \
+#php56-xdebug \
+#php56-wbxml \
 php56-opcache \
-php56-pcntl \
-php56-libevent \
-php56-msgpack
-
+#php56-pcntl \
+#php56-libevent \
 #php56-xcache \
 #php56-gearman \
 #php56-http \
-
-/usr/local/sbin/php56-fpm
-
+php56-msgpack
 
 
+#========================
+#nginx
+brew install nginx
 
-Init_SetDirectoryAndUser 2>&1 | tee -a "${LOGPATH}/1.Init_SetDirectoryAndUser-install.log"
+sudo chown root:wheel /usr/local/Cellar/nginx/1.8.1/bin/nginx
+sudo chmod u+s /usr/local/Cellar/nginx/1.8.1/bin/nginx
 
-Init 2>&1 | tee -a "${LOGPATH}/2.Init-install.log"
+mv /usr/local/etc/nginx/nginx.conf /usr/local/etc/nginx/nginx.conf.old
 
-{ 
- 
-Init_CheckAndDownloadFiles;
+cd $IN_PWD
+sudo cp -rf nginx.conf /usr/local/etc/nginx/
 
-Install_DependsAndOpt;
+#cp -rf conf.default.conf $IN_DIR/vhost/default.conf
+cp -rf conf.upstream.conf $IN_DIR/vhost/upstream.conf
+cp -rf action.nginx $IN_DIR/nginx
 
-if [ $SERVER == "apache" ]; then
-	Install_Apache;
+chmod +x $IN_DIR/nginx
+
+
+
+php_ini="/usr/local/etc/php/5.6/php.ini"
+ln -s $php_ini /private/etc/php.ini
+cp $php_ini "${php_ini}.old"
+
+
+sed -i 's/post_max_size = 8M/post_max_size = 50M/g' $php_ini
+sed -i 's/upload_max_filesize = 2M/upload_max_filesize = 50M/g' $php_ini
+sed -i 's/;date.timezone =/date.timezone = PRC/g' $php_ini
+sed -i 's/short_open_tag = Off/short_open_tag = On/g' $php_ini
+sed -i 's/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/g' $php_ini
+sed -i 's/;cgi.fix_pathinfo=0/cgi.fix_pathinfo=0/g' $php_ini
+sed -i 's/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/g' $php_ini
+sed -i 's/max_execution_time = 30/max_execution_time = 300/g' $php_ini
+sed -i 's/register_long_arrays = On/;register_long_arrays = On/g' $php_ini
+sed -i 's/magic_quotes_gpc = On/;magic_quotes_gpc = On/g' $php_ini
+#sed -i 's/disable_functions =.*/disable_functions = passthru,exec,system,chroot,chgrp,chown,shell_exec,proc_open,proc_get_status,ini_alter,ini_restore,dl,openlog,syslog,readlink,symlink,popepassthru,stream_socket_server/g' $php_ini
+#sed -i 's:mysql.default_socket =:mysql.default_socket ='$IN_DIR'/mysql/data/mysql.sock:g' $php_ini
+#sed -i 's:pdo_mysql.default_socket.*:pdo_mysql.default_socket ='$IN_DIR'/mysql/data/mysql.sock:g' $php_ini
+sed -i 's/expose_php = On/expose_php = Off/g' $php_ini
+
+#sed -i 's#\[opcache\]#\[opcache\]\nzend_extension=opcache.so#g' $php_ini
+sed -i 's/;opcache.enable=0/opcache.enable=0/g' $php_ini
+#sed -i 's/;opcache.enable_cli=0/opcache.enable_cli=1/g' $php_ini
+#sed -i 's/;opcache.memory_consumption=64/opcache.memory_consumption=128/g' $php_ini
+#sed -i 's/;opcache.interned_strings_buffer=4/opcache.interned_strings_buffer=8/g' $php_ini
+#sed -i 's/;opcache.max_accelerated_files=2000/opcache.max_accelerated_files=4000/g' $php_ini
+#sed -i 's/;opcache.revalidate_freq=2/opcache.revalidate_freq=60/g' $php_ini
+#sed -i 's/;opcache.fast_shutdown=0/opcache.fast_shutdown=1/g' $php_ini
+#sed -i 's/;opcache.save_comments=1/opcache.save_comments=0/g' $php_ini
+
+ln -s /usr/local/sbin/php56-fpm $IN_DIR/php-fpm
+
+conf=/usr/local/etc/php/5.6/php-fpm.conf
+cp -rf $conf "${conf}.old"
+ln -s $conf /private/etc/php-fpm.conf
+
+#sed -i 's:;pid = run/php-fpm.pid:pid = run/php-fpm.pid:g' $conf
+sed -i 's:;error_log = log/php-fpm.log:error_log = '"$IN_WEB_LOG_DIR"'/php-fpm.log:g' $conf
+sed -i 's:;log_level = notice:log_level = notice:g' $conf
+sed -i 's:pm.max_children = 5:pm.max_children = 10:g' $conf
+sed -i 's:pm.max_spare_servers = 3:pm.max_spare_servers = 6:g' $conf
+sed -i 's:;request_terminate_timeout = 0:request_terminate_timeout = 100:g' $conf
+sed -i 's/127.0.0.1:9000/127.0.0.1:9950/g' $conf
+
+
+if [ -s "/Users/${USER}/.bashrc" ]; then
+    echo " .bashrc FIND "
 else
-	Install_Nginx;
+    cd $IN_PWD
+    cp -rf .bashrc /Users/${USER}/.bashrc
 fi
 
-Install_PHP;
+if [ -s "/Users/${USER}/.bash_profile" ]; then
+    echo " .bash_profile FIND "
+else
+    cd $IN_PWD
+    cp -rf .bashrc /Users/${USER}/.bash_profile
+fi
 
-Install_PHP_Tools;
-
-Install_Memcached;
-
-Install_Mysql;
-
-Install_Sphinx;
-
- }  2>&1 | tee -a "${LOGPATH}/3.Install.log"
-
-Starup 2>&1 | tee -a "${LOGPATH}/9.Starup-install.log"
-
-CheckInstall 2>&1 | tee -a "${LOGPATH}/10.CheckInstall-install.log"
+echo "OK"
