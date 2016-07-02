0d4e053c837a957130e803d508fa4b0126eaacaf
fox <80005368@qq.com>
2016-07-02 16:05:14 +0800

    php54

--- lib/Install_PHP_5.4.x.sh
+++ lib/Install_PHP_5.4.x.sh
@@ -1,5 +1,5 @@
 
-
+PHP_PATH=${IN_DIR}/php${VERS['php5.4.x']}
 tmp_configure=""
 if [ $SERVER == "nginx" ]; then
 	tmp_configure="--enable-fpm --with-fpm-user=www --with-fpm-group=www"
@@ -12,8 +12,8 @@ echo "php-${VERS['php5.4.x']}.tar.gz"
 cd $IN_DOWN
 tar zxvf php-${PHP_VER}.tar.gz
 cd php-${PHP_VER}/
-./configure --prefix="${IN_DIR}/php" \
---with-config-file-path="${IN_DIR}/php" \
+./configure --prefix="${PHP_PATH}" \
+--with-config-file-path="${PHP_PATH}" \
 --with-mysql=mysqlnd \
 --with-mysqli=mysqlnd \
 --with-pdo-mysql=mysqlnd \
@@ -53,14 +53,25 @@ make install
 #	--enable-magic-quotes 
 #	--enable-safe-mode 
 #	--with-curlwrappers 
-[ -e /usr/bin/php ] && rm -f "/usr/bin/php"
-ln -s "${IN_DIR}/php/bin/php" /usr/bin/php
-ln -s "${IN_DIR}/php/bin/phpize" /usr/bin/phpize
-ln -s "${IN_DIR}/php/sbin/php-fpm" /usr/bin/php-fpm
 
-php_ini="${IN_DIR}/php/php.ini"
+rm -rf "/usr/bin/php54"
+rm -rf "/usr/bin/phpize54"
+rm -rf "/usr/bin/php-fpm54"
+
+ln -s "${PHP_PATH}/bin/php" /usr/bin/php54
+ln -s "${PHP_PATH}/bin/phpize" /usr/bin/phpize54
+ln -s "${PHP_PATH}/sbin/php-fpm" /usr/bin/php-fpm54
+
+if [ -e /usr/bin/php ]; then
+
+else
+    ln -s "${PHP_PATH}/bin/php" /usr/bin/php
+    ln -s "${PHP_PATH}/bin/phpize" /usr/bin/phpize
+    ln -s "${PHP_PATH}/sbin/php-fpm" /usr/bin/php-fpm
+fi
+
+php_ini="${PHP_PATH}/php.ini"
 echo "Copy new php configure file. $php_ini "
-#mkdir -p "${IN_DIR}/etc"
 cp php.ini-production $php_ini
 
 echo "Modify php.ini......"
@@ -79,36 +90,30 @@ sed -i 's:mysql.default_socket =:mysql.default_socket ='$IN_DIR'/mysql/data/mysq
 sed -i 's:pdo_mysql.default_socket.*:pdo_mysql.default_socket ='$IN_DIR'/mysql/data/mysql.sock:g' $php_ini
 sed -i 's/expose_php/;expose_php/g' $php_ini
 
-ln -s $php_ini $IN_DIR/etc/php.ini
-
 #PHP-FPM
 if [ $SERVER == "nginx" ]; then
 
 
 
-echo "MV php-fpm.conf file"
-conf=$IN_DIR/php/etc/php-fpm.conf;
-mv $IN_DIR/php/etc/php-fpm.conf.default $conf
+        echo "MV php-fpm.conf file"
+        conf=$PHP_PATH/etc/php-fpm.conf;
+        mv $PHP_PATH/etc/php-fpm.conf.default $conf
 
-sed -i 's:;pid = run/php-fpm.pid:pid = run/php-fpm.pid:g' $conf
-sed -i 's:;error_log = log/php-fpm.log:error_log = '"$IN_WEB_LOG_DIR"'/php-fpm.log:g' $conf
-sed -i 's:;log_level = notice:log_level = notice:g' $conf
-sed -i 's:pm.max_children = 5:pm.max_children = 10:g' $conf
-sed -i 's:pm.max_spare_servers = 3:pm.max_spare_servers = 6:g' $conf
-sed -i 's:;request_terminate_timeout = 0:request_terminate_timeout = 100:g' $conf
-sed -i 's/127.0.0.1:9000/127.0.0.1:9950/g' $conf
+        sed -i 's:;pid = run/php-fpm.pid:pid = run/php-fpm.pid:g' $conf
+        sed -i 's:;error_log = log/php-fpm.log:error_log = '"$IN_WEB_LOG_DIR"'/php-fpm.log:g' $conf
+        sed -i 's:;log_level = notice:log_level = notice:g' $conf
+        sed -i 's:pm.max_children = 5:pm.max_children = 10:g' $conf
+        sed -i 's:pm.max_spare_servers = 3:pm.max_spare_servers = 6:g' $conf
+        sed -i 's:;request_terminate_timeout = 0:request_terminate_timeout = 100:g' $conf
+        sed -i 's/127.0.0.1:9000/127.0.0.1:9950/g' $conf
 
-ln -s $IN_DIR/php/etc/php-fpm.conf $IN_DIR/etc/php-fpm.conf
+        echo "Copy php-fpm init.d file......"
+        cp "${IN_DOWN}/php-${PHP_VER}/sapi/fpm/init.d.php-fpm" $IN_DIR/bin/php-fpm54
+        chmod +x $IN_DIR/bin/php-fpm54
 
-echo "Copy php-fpm init.d file......"
-cp "${IN_DOWN}/php-${PHP_VER}/sapi/fpm/init.d.php-fpm" $IN_DIR/action/php-fpm
-chmod +x $IN_DIR/action/php-fpm
-if [ $ETC_INIT_D_LN = 1 ]; then
-	ln -s $IN_DIR/action/php-fpm $IN_DIR/init.d/php-fpm
-fi
-if [ ! $IN_DIR = "/www/lanmps" ]; then
-	sed -i "s:/www/lanmps:$IN_DIR:g" $IN_DIR/action/php-fpm
-fi
+        if [ ! $IN_DIR = "/www/lanmps" ]; then
+            sed -i "s:/www/lanmps:$IN_DIR:g" $IN_DIR/bin/php-fpm
+        fi
 
 fi
 #PHP-FPM
